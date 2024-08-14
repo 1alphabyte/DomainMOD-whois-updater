a047b865dcfb7b07930143a8a9dc02fd42a216e5
Alphabyte <github@utsav2.dev>
2024-08-13 19:44:43 -0700

    Complete go code

--- go.mod
+++ go.mod
@@ -2,6 +2,15 @@ module github.com/1alphabyte/domainmod-whois-updater
 
 go 1.22.6
 
-require github.com/go-sql-driver/mysql v1.8.1
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/likexian/whois v1.15.4
+	github.com/likexian/whois-parser v1.24.19
+)
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/likexian/gokit v0.25.15 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,39 +2,110 @@ package main
 
 import (
 	"database/sql"
+	"log"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/likexian/whois"
+	whoisparser "github.com/likexian/whois-parser"
 )
 
-func main() {
-	db, err := sql.Open("mysql", "domainmod:{PASSWORD}@db/domainmod")
+type Domain struct {
+	ID      int
+	Domain  string
+	ExpDate string
+}
+
+func setUpDB() *sql.DB {
+	db, err := sql.Open("mysql", "domainmod:vVZraEnJsuYz37J5Ge0K@tcp(db:3306)/domainmod")
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
-	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetConnMaxLifetime(time.Minute * 4)
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
-	defer db.Close()
-	//  get domains colum from a db table calls domains and put it in a array
-	rows, err := db.Query("SELECT domain FROM domains")
+	return db
+}
+
+func getDomains(db *sql.DB) []Domain {
+	rows, err := db.Query("SELECT domain, id, expiry_date FROM domains")
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	defer rows.Close()
-	var domains []string
+	var domains []Domain
 	for rows.Next() {
-		var domain string
-		if err := rows.Scan(&domain); err != nil {
-			panic(err)
+		var domain, expiry_date string
+		var id int
+		if err := rows.Scan(&domain, &id, &expiry_date); err != nil {
+			log.Panic("failed to get domains from DB: ", err)
 		}
-		domains = append(domains, domain)
+		domains = append(domains, Domain{Domain: domain, ID: id, ExpDate: expiry_date})
 	}
 	if err := rows.Err(); err != nil {
-		panic(err)
+		log.Panic(err)
 	}
-	// print the array
-	for _, domain := range domains {
-		println(domain)
+	return domains
+}
+
+func main() {
+	for {
+		db := setUpDB()
+		domains := getDomains(db)
+
+		currentDate := time.Now()
+		for _, domain := range domains {
+			ExpDate, err := time.Parse("2006-01-02", domain.ExpDate)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			diff := ExpDate.Sub(currentDate).Hours() / 24
+			log.Println(domain.Domain)
+			if int(diff) <= 25 {
+				result, err := whois.Whois(domain.Domain)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				res, err := whoisparser.Parse(result)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				log.Println(strings.Split(res.Domain.ExpirationDate, "T")[0], res.Registrar.Name, res.Domain.NameServers)
+				var regID int
+				rows, err := db.Query("SELECT id FROM registrars WHERE name = ?", res.Registrar.Name)
+				if err != nil && err != sql.ErrNoRows {
+					log.Println(err)
+					continue
+				} else if err == sql.ErrNoRows {
+					row, err := db.Exec("INSERT INTO registrars (name, url, notes) VALUES (?, ?, 'None')", res.Registrar.Name, res.Registrar.ReferralURL)
+					if err != nil {
+						log.Println(err)
+						continue
+					}
+					id, err := row.LastInsertId()
+					if err != nil {
+						log.Println(err)
+						continue
+					}
+					regID = int(id)
+				}
+				rows.Scan(&regID)
+				r, err := db.Exec("UPDATE domains SET expiry_date = ?, registrar_id = ?, update_time = ? WHERE id = ?", strings.Split(res.Domain.ExpirationDate, "T")[0], regID, time.Now().Format("2006-01-02"), domain.ID)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				r1, _ := r.RowsAffected()
+				log.Println("Updated", r1)
+				time.Sleep(20 * time.Second)
+			}
+		}
+		db.Close()
+		// sleep ~10 days
+		time.Sleep(220 * time.Hour)
 	}
 }
